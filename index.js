36dcbd586caabaf300c81abb88224dd5e1d360cf
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2023-03-17 22:29:04 +0600

    update

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const vocabularyRoute = require('./routes/v1/vocabulary.route');
 const postRoutes = require('./routes/v1/post.route')
 const userRoute = require('./routes/v1/user.route');
 
-// twilio
-/* const Account_SID = 'AC480f92548518d3e7855cca4be6298f8d'
-const Auth_Token = '621c63220d643a952009038049a44c05';
-const client = require('twilio')(Account_SID, Auth_Token); */
 
 // middleware
 app.use(cors())
@@ -23,7 +19,7 @@ app.use(express.json())
 
 
 // database connected
-mongoose.connect(`${process.env.DATABASE_LOCAL}`)
+mongoose.connect(`${process.env.DATABASE_URL}`)
     .then(() => console.log('database is connected'))
     .catch(() => console.log('database is not connected'))
 
@@ -32,15 +28,6 @@ app.use('/api/v1/vocabulary', vocabularyRoute); // vocabulary api
 app.use('/api/v1/user', userRoute) // user singup and loging api
 app.use('/api/v1/userPost', postRoutes)
 
-/* client.messages
-    .create({
-        body: 'Abu default requestClient!',
-        from: '+17122141267',
-        to: '+8801857927912',
-    })
-    .then(message => console.log(`Message SID ${message.sid}`))
-    .catch(error => console.error('inside', error));
- */
 
 
 app.get('/', (req, res) => {

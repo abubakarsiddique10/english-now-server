bef41b288112d7ff2532fa89152e724eb3bdd5d0
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2023-04-18 12:18:31 +0600

    update

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -62,14 +62,16 @@ module.exports.updateLikeCount = async (req, res) => {
     try {
         const { postId } = req.params;
         const { userId } = req.body;
+
         const existId = await UsersPosts.find({ $and: [{ _id: postId }, { likes: userId }] });
 
         if (existId.length) {
             return res.send({ status: false, message: "You have already liked this post" })
         }
 
-        const updateLike = await UsersPosts.updateOne({ _id: postId }, { $push: { 'likes': userId } });
-        res.status(200).send({ status: true, message: "successful" })
+        const updateLike = await UsersPosts.findOneAndUpdate({ _id: postId }, { $push: { 'likes': userId } }, { new: true });
+        res.status(200).send({ status: true, likes: updateLike.likes, id: updateLike._id })
+
     } catch (error) {
         res.status(301).send({ status: false })
     }
